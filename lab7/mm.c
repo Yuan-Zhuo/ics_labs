612419f617add4afc8247829ef553b87833e1bf7
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2019-03-12 23:12:26 +0800

    Finish simple mm_init and mm_free.

--- lab7/mm.c
+++ lab7/mm.c
@@ -26,10 +26,65 @@
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* $begin mallocmacros */
+/* Basic constants and macros */
+#define WSIZE \
+    4 /* Word and header/footer size (bytes) */  // line:vm:mm:beginconst
+#define DSIZE 8                                  /* Double word size (bytes) */
+#define CHUNKSIZE \
+    (1 << 12) /* Extend heap by this amount (bytes) */  // line:vm:mm:endconst
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))  // line:vm:mm:pack
+
+/* Read and write a word at address p */
+#define GET(p) (*(unsigned int*)(p))               // line:vm:mm:get
+#define PUT(p, val) (*(unsigned int*)(p) = (val))  // line:vm:mm:put
+
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p) (GET(p) & ~0x7)  // line:vm:mm:getsize
+#define GET_ALLOC(p) (GET(p) & 0x1)  // line:vm:mm:getalloc
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp) ((char*)(bp)-WSIZE)                         // line:vm:mm:hdrp
+#define FTRP(bp) ((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)  // line:vm:mm:ftrp
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp) \
+    ((char*)(bp) + GET_SIZE(((char*)(bp)-WSIZE)))  // line:vm:mm:nextblkp
+#define PREV_BLKP(bp) \
+    ((char*)(bp)-GET_SIZE(((char*)(bp)-DSIZE)))  // line:vm:mm:prevblkp
+/* $end mallocmacros */
+
+/* Global variables */
+static char* heap_listp = 0; /* Pointer to first block */
+
+/* Function prototypes for internal helper routines */
+static void* extend_heap(size_t words);
+static void place(void* bp, size_t asize);
+static void* find_fit(size_t asize);
+static void* coalesce(void* bp);
+static void printblock(void* bp);
+static void checkheap(int verbose);
+static void checkblock(void* bp);
+
 /*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void) {
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void*)(-1))
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+    heap_listp += (2 * WSIZE);
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
+
     return 0;
 }
 
@@ -51,7 +106,13 @@ void* mm_malloc(size_t size) {
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void* ptr) {}
+void mm_free(void* bp) {
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
+}
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
@@ -70,4 +131,43 @@ void* mm_realloc(void* ptr, size_t size) {
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
+}
+
+static void* extend_heap(size_t words) {
+    char* bp;
+    size_t size;
+
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
+}
+
+static void* coalesce(void* bp) {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(bp);
+
+    if (prev_alloc && next_alloc) {
+        return bp;
+    } else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    } else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    } else {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(FTRP(PREV_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
 }
