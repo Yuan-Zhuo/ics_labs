5bf4757a3d3fa6c6d3af70b1db4655b8ac60185b
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2019-03-28 00:20:05 +0800

    Finish part b.

--- lab8/csim.c
+++ lab8/csim.c
@@ -1,3 +1,8 @@
+/*
+ *  loginID: 517030910169
+ *  name: yuanzhuo
+ */
+
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -6,17 +11,50 @@
 #include <unistd.h>
 #include "cachelab.h"
 
+/**
+ *  basic logic:
+ *      1.valid bit: present by an unsigned int where 0 means invalid, and 1~inf
+ *                   indicates the last time when this block accessed.
+ *      2.tag bits: present by a long.
+ *      3.just malloc for the above bits, so for one block, malloc 12byte.
+ */
+
+/**
+ *  magic number:
+ *      maxLine: the max size of a line reading from .trace
+ *      addrOff: the offset of the address in a line
+ *      addrBits: the address bits
+ *      lineSize: malloc size
+ */
 #define maxLine 32
 #define addrOff 3
 #define addrBits 64
 #define lineSize 12
 
+/*
+ *  tool function:
+ *      validSpace: weither this block is valid
+ *      boolSymbol: weither the data in this line need 'M'(modify)
+ *      getSize: find out how much sets, lines should be malloced by bits
+ *      splitAddr: split the address into tag, valid
+ */
 #define validSpace (sizeof(bool))
 #define boolSymbol(ch) (ch == 'M')
 #define getSize(bits) (1 << (bits))
 #define splitAddr(addr, lowestbit, bits) \
     ((addr) >> (lowestbit)) & ~((~0) << (bits))
 
+/*
+ *  static variable:
+ *      setBits: s
+ *      lines: E
+ *      blkBits: b
+ *      tagBits: t
+ *      hits: the sum of hit times
+ *      misses: the sum of miss times
+ *      evicts: the sum of eviction times
+ *      showDetail: if -v in args
+ */
 static int setBits;
 static int lines;
 static int blkBits;
@@ -26,6 +64,10 @@ static int misses = 0;
 static int evicts = 0;
 static bool showDetail = false;
 
+/*
+ *  initCond- read the args from command line and
+ *            initialize the info of the cache
+ */
 char* initCond(int argc, char* argv[]) {
     int opt;
     const char optStr[] = "vs:E:b:t:";
@@ -55,10 +97,17 @@ char* initCond(int argc, char* argv[]) {
     return filename;
 }
 
+/*
+ *  simTrace- handle a single data
+ */
 void simTrace(void* CACHE_ptr, bool flag, long addr) {
+    // count the times of simTrace called
+    // assign to the valid space then
+    // while the min nonzero valid represent least-recently used
     static unsigned cnt = 0;
     cnt++;
 
+    // get the info of the data from addr
     int set = splitAddr(addr, blkBits, setBits);
     long tag = splitAddr(addr, blkBits + setBits, tagBits);
 
@@ -69,10 +118,12 @@ void simTrace(void* CACHE_ptr, bool flag, long addr) {
 
     void* cur_addr = Set_ptr - lineSize;
 
+    // traverse the all lines in a specific set
     for (int i = 0; i < lines; ++i) {
         cur_addr += lineSize;
         unsigned cur_valid = *(unsigned int*)cur_addr;
 
+        // invalid line
         if (cur_valid == 0) {
             if (illegal_index == -1)
                 illegal_index = i;
@@ -80,44 +131,84 @@ void simTrace(void* CACHE_ptr, bool flag, long addr) {
         }
 
         long cur_tag = *(long*)(cur_addr + 4);
+        // hit
         if (cur_tag == tag) {
             hits++;
+            // update to lastest used
             *(unsigned int*)(cur_addr) = cnt;
 
+            // hit two times if the symbol is 'M'
             if (flag)
                 hits++;
 
             return;
         }
 
+        // record the index of the min valid
         if (min_valid > cur_valid) {
             min_valid = cur_valid;
             min_index = i;
         }
     }
 
+    // not found, therefore miss
     misses++;
 
     int fill_index = illegal_index;
+    // no invalid block, so evict LRU
     if (illegal_index == -1) {
         evicts++;
 
         fill_index = min_index;
     }
 
+    // hit one times if the symbol is 'M'
     if (flag) {
         hits++;
         cnt++;
     }
 
+    // replace
     *(unsigned int*)(Set_ptr + fill_index * lineSize) = cnt;
     *(long*)(Set_ptr + fill_index * lineSize + 4) = tag;
 
     return;
 }
 
+/*
+ *  display- display the status of when a single data accessed
+ */
+void display(char* strLine, bool isHit, bool isEvict) {
+    if (!showDetail)
+        return;
+
+    // copy and remove the '\n' in strLine
+    char info[100];
+    char* ch = info;
+    while ((*ch++ = *strLine++))
+        ;
+
+    ch -= 2;
+    *ch++ = '\t';
+    *ch = 0;
+
+    if (isHit)
+        strcat(info, "Hit\t");
+    else
+        strcat(info, "Miss\t");
+
+    if (isEvict)
+        strcat(info, "Eviction\t");
+
+    printf("%s\n", info);
+}
+
+/*
+ *  simulator- read the file and handle every line
+ */
 bool simulator(char* filename, void* CACHE_ptr) {
     FILE* fp = NULL;
+    // read exception
     if ((fp = fopen(filename, "r")) == NULL) {
         printf("cannot open file: %s", filename);
         return false;
@@ -131,13 +222,19 @@ bool simulator(char* filename, void* CACHE_ptr) {
         if (feof(fp))
             break;
 
+        // if it's an 'I'
         if (strLine[0] != ' ')
             continue;
 
         int len = strlen(strLine);
         strncpy(strAddr, strLine + addrOff, len);
 
+        int oldHit = hits, oldEvict = evicts;
+        // handle this line
         simTrace(CACHE_ptr, boolSymbol(strLine[1]), strtol(strAddr, NULL, 16));
+
+        // call to display the status
+        display(strLine, hits == oldHit, evicts == oldEvict);
     }
 
     free(strLine);
@@ -149,9 +246,11 @@ int main(int argc, char* argv[]) {
     char* filename = initCond(argc, argv);
 
     int setNum = getSize(setBits);
+    // malloc the space to represent cache
     void* CACHE_ptr = (void*)malloc(lineSize * setNum * lines);
     void* cur_ptr = CACHE_ptr;
 
+    // init every block to invalid
     for (int i = 0; i < setNum; ++i) {
         for (int j = 0; j < lines; ++j) {
             *(unsigned int*)cur_ptr = 0;
@@ -159,6 +258,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // handle the trace file
     if (!simulator(filename, CACHE_ptr))
         return 1;
 
--- lab8/trans.c
+++ lab8/trans.c
@@ -1,40 +1,52 @@
-/* 
- * trans.c - Matrix transpose B = A^T
- *
- * Each transpose function must have a prototype of the form:
- * void trans(int M, int N, int A[N][M], int B[M][N]);
- *
- * A transpose function is evaluated by counting the number of misses
- * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+/*
+ *  loginID: 517030910169
+ *  name: yuanzhuo
+ */
+
 #include <stdio.h>
 #include "cachelab.h"
 
+/**
+ *  basic logic:
+ *      1.base on waside-blocking.pdf
+ *      2.write the specific function for these cases showing below
+ *      3.get the conflict message between cache for A and B
+ *
+ *      int x = 0x30a100;
+ *      int xx = 0x34a100;
+ *      for(int i = 0; i < 64 * 64; i++){
+ *             long y = xx + 4 * i;
+ *             if((y >> 5U) % 32U == 9)
+ *                 printf("%d\t", i);
+ *      }
+ *      4.for the speficit conflict, store tmp in vals to avoid
+ */
+
+void trans(int M, int N, int A[N][M], int B[M][N]);
+void trans_32_32(int M, int N, int A[N][M], int B[M][N]);
+void trans_64_64(int M, int N, int A[N][M], int B[M][N]);
+void trans_61_67(int M, int N, int A[N][M], int B[M][N]);
+
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
- * transpose_submit - This is the solution transpose function that you
- *     will be graded on for Part B of the assignment. Do not change
- *     the description string "Transpose submission", as the driver
- *     searches for that string to identify the transpose function to
- *     be graded. 
+/*
+ * transpose_submit - three special cases and the common case
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    if (M == 32 && N == 32) {
+        trans_32_32(32, 32, A, B);
+    } else if (M == 64 && N == 64) {
+        trans_64_64(64, 64, A, B);
+    } else if (M == 61 && N == 67) {
+        trans_61_67(61, 67, A, B);
+    } else {
+        trans(M, N, A, B);
+    }
 }
 
-/* 
- * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
-
-/* 
- * trans - A simple baseline transpose function, not optimized for the cache.
- */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
+void trans(int M, int N, int A[N][M], int B[M][N]) {
     int i, j, tmp;
 
     for (i = 0; i < N; i++) {
@@ -42,43 +54,171 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
- * registerFunctions - This function registers your transpose
- *     functions with the driver.  At runtime, the driver will
- *     evaluate each of the registered functions and summarize their
- *     performance. This is a handy way to experiment with different
- *     transpose strategies.
+ * transpose_32_32 - Because a single block with 32 byte can store 8 int, so the
+ *      max basic block is 8*8.
+ *      To avoid conflict, just lazy the assignment to axle wire elements.
  */
-void registerFunctions()
-{
-    /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+char trans_32_32_desc[] = "32x32-transpose";
+void trans_32_32(int M, int N, int A[N][M], int B[M][N]) {
+    int blkRow, blkCol;
+    int eleRow, eleCol;
+    int equalIdx;
+    int tmp0;
 
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    for (blkRow = 0; blkRow < 4; ++blkRow) {
+        for (blkCol = 0; blkCol < 4; ++blkCol) {
+            for (eleRow = 8 * blkRow; eleRow < 8 * blkRow + 8; ++eleRow) {
+                for (eleCol = 8 * blkCol; eleCol < 8 * blkCol + 8; ++eleCol) {
+                    if (eleRow != eleCol)
+                        B[eleCol][eleRow] = A[eleRow][eleCol];
+                    else {
+                        equalIdx = eleRow;
+                        tmp0 = A[eleRow][eleCol];
+                    }
+                }
+                if (blkRow == blkCol)
+                    B[equalIdx][equalIdx] = tmp0;
+            }
+        }
+    }
 }
 
-/* 
- * is_transpose - This helper function checks if B is the transpose of
- *     A. You can check the correctness of your transpose by calling
- *     it before returning from the transpose function.
+/*
+ * transpose_64_64 - Divide the 8*8 block into three,
+ *                   and handle flow is 8*4 to 4*4
  */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
+char trans_64_64_desc[] = "64x64-transpose";
+void trans_64_64(int M, int N, int A[N][M], int B[M][N]) {
+    // index
+    int i, j, ii, jj;
+    // tmps
+    int val0, val1, val2, val3, val4, val5, val6, val7;
+    for (ii = 0; ii < N; ii += 8) {
+        for (jj = 0; jj < M; jj += 8) {
+            // For each row in the 8*4 block
+            for (i = 0; i < 4; i++) {
+                val0 = A[ii + i][jj + 0];
+                val1 = A[ii + i][jj + 1];
+                val2 = A[ii + i][jj + 2];
+                val3 = A[ii + i][jj + 3];
+                val4 = A[ii + i][jj + 4];
+                val5 = A[ii + i][jj + 5];
+                val6 = A[ii + i][jj + 6];
+                val7 = A[ii + i][jj + 7];
+                B[jj + 0][ii + i] = val0;
+                B[jj + 1][ii + i] = val1;
+                B[jj + 2][ii + i] = val2;
+                B[jj + 3][ii + i] = val3;
+                B[jj + 0][ii + 4 + i] = val4;
+                B[jj + 1][ii + 4 + i] = val5;
+                B[jj + 2][ii + 4 + i] = val6;
+                B[jj + 3][ii + 4 + i] = val7;
+            }
+            // First copy the first 4 rows
+            for (i = 0; i < 4; i++) {
+                // get this row of the right-upper 4*4 block
+                val0 = B[jj + i][ii + 4];
+                val1 = B[jj + i][ii + 5];
+                val2 = B[jj + i][ii + 6];
+                val3 = B[jj + i][ii + 7];
+                // update this row to its correct value
+                val4 = A[ii + 4][jj + i];
+                val5 = A[ii + 5][jj + i];
+                val6 = A[ii + 6][jj + i];
+                val7 = A[ii + 7][jj + i];
+
+                B[jj + i][ii + 4] = val4;
+                B[jj + i][ii + 5] = val5;
+                B[jj + i][ii + 6] = val6;
+                B[jj + i][ii + 7] = val7;
+
+                // update the left lower 4*4 block of B
+                B[jj + 4 + i][ii + 0] = val0;
+                B[jj + 4 + i][ii + 1] = val1;
+                B[jj + 4 + i][ii + 2] = val2;
+                B[jj + 4 + i][ii + 3] = val3;
+            }
+
+            for (i = 4; i < 8; i++)
+                for (j = 4; j < 8; j++)
+                    B[jj + j][ii + i] = A[ii + i][jj + j];
+        }
+    }
+}
+
+/*
+ * transpose_61_67 - The same logic as 32*32, but the basic block is 4*4.
+ *      To avoid conflict, just lazy the assignment to axle wire elements.
+ */
+char trans_61_67_desc[] = "61x67-transpose";
+void trans_61_67(int M, int N, int A[N][M], int B[M][N]) {
+    int blkRow, blkCol;
+    int eleRow, eleCol;
+    int equalIdx;
+    int tmp0;
+
+    for (blkCol = 0; blkCol < 61; blkCol += 16) {
+        for (blkRow = 0; blkRow < 67; blkRow += 16) {
+            for (eleRow = blkRow; (eleRow < 67) && (eleRow < blkRow + 16);
+                 eleRow++) {
+                for (eleCol = blkCol; (eleCol < 61) && (eleCol < blkCol + 16);
+                     eleCol++) {
+                    if (eleRow != eleCol) {
+                        B[eleCol][eleRow] = A[eleRow][eleCol];
+                    }
+
+                    else {
+                        tmp0 = A[eleRow][eleCol];
+                        equalIdx = eleRow;
+                    }
+                }
+
+                if (blkRow == blkCol) {
+                    B[equalIdx][equalIdx] = tmp0;
+                }
+            }
+        }
+    }
+}
+
+/*
+ * registerFunctions - trans
+ *                   - trans_32_32
+ *                   - trans_64_64
+ *                   - trans_61_67
+ */
+void registerFunctions() {
+    /* Register your solution function */
+    registerTransFunction(transpose_submit, transpose_submit_desc);
+
+    registerTransFunction(trans, trans_desc);
+
+    registerTransFunction(trans_32_32, trans_32_32_desc);
+
+    registerTransFunction(trans_64_64, trans_64_64_desc);
+
+    registerTransFunction(trans_61_67, trans_61_67_desc);
+}
+
+/*
+ * is_transpose- display the differences also
+ */
+void is_transpose(int M, int N, int A[N][M], int B[M][N]) {
     int i, j;
 
     for (i = 0; i < N; i++) {
         for (j = 0; j < M; ++j) {
             if (A[i][j] != B[j][i]) {
-                return 0;
+                printf("%d!!%d\t", A[i][j], B[i][j]);
+            } else {
+                printf("%d\t", A[i][j]);
             }
         }
+        printf("\n");
     }
-    return 1;
+    return;
 }
-
